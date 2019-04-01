bd1b162d7e0fc608acaf37d0fedb3bc705276b94
Unknown <brandon290697@gmail.com>
2019-04-01 16:53:27 -0600

    Revert "Actualización de directorio completo"
    This reverts commit 6c67d32e50faf206f6381c8f1508583d3fc1e4d9.

--- appsocial/resources/views/welcome.blade.php
+++ appsocial/resources/views/welcome.blade.php
@@ -3,65 +3,19 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        
+        <link href="{{ asset('css/Botones.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/Cuerpo.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/Inputs/Inputs.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/Inputs/normalizarInputs.css') }}" rel="stylesheet">
+        <link href="{{ asset('fonts/font-awesome-4.2.0/css/font-awesome.min.css') }}" rel="stylesheet">
 
-        <title>Laravel</title>
+        <title>App Social</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
     <body>
         <div class="flex-center position-ref full-height">
@@ -79,21 +33,29 @@
                 </div>
             @endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+            <div class="container">
+            <section class="content">
+                <img src="img/Logo-Imagen.png" class="img-responsive" width="30%" height="30%">
+                <br>
+                <span class="input" id="inputLogin">
+                    <input id="inputFieldLogin" class="inputField" type="text" id="correo" placeholder="Correo" />
+                    <label class="inputLabel inputLabelAzul" for="email">
+                        <i class="fa fa-fw fa-envelope icono iconoInput"></i>
+                        <span class="inputLabelContenido"></span>
+                    </label>
+                </span>
+                <br>
+                <span class="input" id="inputLogin">
+                    <input class="inputField" type="password" id="pass" placeholder="Contraseña" />
+                    <label class="inputLabel inputLabelAzul" for="contrasena">
+                        <i class="fa fa-fw fa-lock icono iconoInput"></i>
+                        <span class="inputLabelContenido"></span>
+                    </label>
+                </span>
+                <br><br>
+                <button onclick="location.href='menu.html';" class="boton botonAzul">Entrar</button>
+            </section>
+        </div>
         </div>
     </body>
 </html>
